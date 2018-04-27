dd73895b1e93a94aa90451481e298eb5ae70922b
kgourishetty <kgourishetty@KGOURISH-L-1116.evoketechnologies.com>
2018-04-27 17:34:37 +0530

    Commit on 27/04

--- com/main/scala/com/saprkscala/practice/job/AttendanceJob.scala
+++ com/main/scala/com/saprkscala/practice/job/AttendanceJob.scala
@@ -5,16 +5,25 @@ import org.apache.spark._
 object AttendanceJob {
   def main(args: Array[String]): Unit = {
     println("testing")
+    
+    System.setProperty("hadoop.home.dir", "C://hadoop//")
+    
     var conf = new SparkConf().setAppName("Attendance Agg Job").setMaster("local[*]")
     var sc = new SparkContext(conf)
     
     var list = List(1,2,3,4,5,6)
     
-    var data = sc.parallelize(list)
+  /*  var data = sc.parallelize(list)
     
     println(data.count())
     
-    println(data.collect.toList)
+    println(data.collect.toList)*/
+    
+    var data = sc.textFile("C://hadoop//attendanceSource//")
+    
+    println(data.getNumPartitions)
+    
+    
     
   }
 }
